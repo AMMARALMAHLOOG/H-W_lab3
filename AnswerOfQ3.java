976713c78671135061e78cad495d012148e3b23f
AMMARALMAHLOOG <97596274+AMMARALMAHLOOG@users.noreply.github.com>
2022-02-07 13:00:41 +0300

    Add files via upload

---
+++ AnswerOfQ3.java
@@ -0,0 +1,32 @@
+/**
+ * Created by hp on 07-Feb-22.
+ */
+public class AnswerOfQ3 {
+   int a[]=new int[]{12,13,14,15};
+    public void pop(int d){
+        for (int i = 0; i <a.length ; i++) {
+            System.out.print("["+a[i]+"]");
+        }
+        System.out.println();
+        for (int i = 0; i <a.length ; i++) {
+            if (a[i]==d)
+                a[i]=0;
+            else {
+                System.out.println("Not found");
+            }
+            i=a.length;
+        }
+
+        for (int i = 0; i <a.length; i++) {
+            System.out.print("["+a[i]+"]");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+    AnswerOfQ3 s1=new AnswerOfQ3();
+        s1.pop(12);
+        s1.pop(100);
+    }
+}
